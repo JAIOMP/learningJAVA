aa5f45aa42a3ed04f5bbf64f26ce4735860a3644
Jai Om Pandey <jaiomp@thoughtworks.com>
2016-02-22 00:17:46 +0530

    create a function for  determinant of a matrix

--- Math/MatrixTest.java
+++ Math/MatrixTest.java
@@ -1,5 +1,5 @@
-// import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.*;
 
 
@@ -51,7 +51,17 @@ public class MatrixTest {
 
 		assertArrayEquals(destinationMatrix.matrix,expectedMatrix.matrix);
 
-	} 
+	}
 
+	@Test 
+	public void determinantOfMatrix(){
+    	int[] numbers = {3,4,5,6,7,8,1,9,6};
+    	Matrix matrix = new Matrix(3,3);
+    	matrix.populate(numbers);
+    	int[][] input = matrix.matrix;
+    	int result = matrix.determinant(input,3);
+    	System.out.println(result);
+    	assertEquals(result,33);
+	}
 };
 
