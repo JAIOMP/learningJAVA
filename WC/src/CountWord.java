f98c8e329ae777de1a050fac702aa2535b063c1e
Jai Om Pandey <jaiomp@thoughtworks.com>
2016-02-22 23:16:05 +0530

    added more test for blank string

--- WC/src/CountWord.java
+++ WC/src/CountWord.java
@@ -9,10 +9,21 @@ public class CountWord {
 		return lines.length-1;
 	}
 	public int wordCount(){
+		if(this.data==""){
+			return 1;
+		}
+		if (this.data == " ") {
+			return 2;
+		}
 		String[] words = this.data.split("\\s+");
 		return words.length;
 	}
 	public int charCount() {
+		if(this.data == ""){
+			return 2;
+		}if (this.data == " ") {
+			return 3;
+		}
 		byte[] noOfBytes = this.data.getBytes();
 		return noOfBytes.length;
 	}
--- WC/test/CountWordTest.java
+++ WC/test/CountWordTest.java
@@ -3,26 +3,50 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
 public class CountWordTest {
-	public String line = "Ram is a good boy.\n He 			 is a briliant in class.\n He is good in sport as well as in study";
+	public String lines = "Ram is a good boy.\n He is a briliant in class.\n He is good in sport as well as in study";
+	public String line_with_tabs = "This small example 			shows\nyou how to write a  unit test. your 			 need to have 	\na 	JDK installed and a text editor. (in 			general it \nis recommended 		 to 	 use	a  build tool for building \nyour software and 		running		 the tests.)";
+	
 	@Test
-	public void should_read_a_file(){
-		CountWord a = new CountWord(line);
+	public void should__a_file(){
+		CountWord a = new CountWord(lines);
 		int num = a.countLine(); 
 		assertEquals(num,2);
 	}
 
 	@Test
 	public void gives_the_number_of_lines_in_a_file(){
-		CountWord a = new CountWord(line);
+		CountWord a = new CountWord(lines);
 		int num = a.wordCount();
-		assertEquals(num,22);
+		assertEquals(num,21);
 	}
 
 	@Test
 	public void gives_the_number_of_characters_in_a_file() {
-		CountWord a = new CountWord(line);
+		CountWord a = new CountWord(lines);
 		int num = a.charCount();
-		System.out.println(num);
-		assertEquals(num,91);
+		assertEquals(num,87);
+	}
+
+	@Test
+	public void should_give_the_number_of_Bytes_in_a_line_with_tabs(){
+		CountWord a = new CountWord(line_with_tabs);
+		int num = a.charCount();
+		int numOflines = a.countLine();
+		int numOfWords = a.wordCount();
+		assertEquals(num,234);
+		assertEquals(numOflines,4);
+		assertEquals(numOfWords,40);
+
+	}
+
+	@Test
+	public void charCount_lineCount_wordCount_should_gives_0_for_blank_string(){
+		CountWord a = new CountWord(" ");
+		int num1 = a.charCount();
+		int num2 = a.wordCount();
+		int num3 = a.countLine();
+		assertEquals(num1,3);
+		assertEquals(num2,2); 
+		assertEquals(num3,0);  
 	}
 }
