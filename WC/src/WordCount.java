1410c3f1831d1dec569fb4809f049ef584c5262f
Jai Om Pandey <jaiomp@thoughtworks.com>
2016-02-23 00:25:10 +0530

    isFileExist function created in wordcount which returns boolean value

--- WC/src/WordCount.java
+++ WC/src/WordCount.java
@@ -1,15 +1,33 @@
 import java.io.*;
 
-class WordCount{
+
+class WordCount {
+	public static void check_command(String command){
+		if(command == "-l"){
+
+		}
+		if(command == "-m"){
+
+		}
+		if(command == "-c") {
+			
+		}
+	}
+
+	public static boolean isFileExist(File file){
+		return file.isFile();
+	}
 	public static void main(String[] args) throws IOException {
 		String fileName = args[0];
 		File file = new File(fileName);
 		int size = (int) file.length();
-		FileReader fr = new FileReader(file);
-		char[] fileContents = new char[size];
-		int buffer = fr.read(fileContents,0,size);
-		CountWord wc = new CountWord(new String(fileContents));
-		System.out.println("\t"+wc.countLine()+"\t"+wc.wordCount()+"\t"+wc.charCount()+"\t"+fileName);	
-		fr.close();
+		if(isFileExist(file)){
+			FileReader fr = new FileReader(file);
+			char[] fileContents = new char[size];
+			int buffer = fr.read(fileContents,0,size);
+			CountWord wc = new CountWord(new String(fileContents));
+			System.out.println("\t"+wc.countLine()+"\t"+wc.wordCount()+"\t"+wc.charCount()+"\t"+fileName);	
+			fr.close();			
+		}
 	}
 }
